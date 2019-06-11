250ba129a2c8923e40919d50a88305e2e82db30e
kiran malsetty <malsetty.kiran@gmail.com>
2019-06-11 11:09:46 -0400

    Adding Shell script files

---
+++ AWS-Linux-Metrics-Task/awspush.sh
@@ -0,0 +1,1 @@
+aws s3 cp /home/ubuntu/metrics/metrics s3://kiran-bucket-card/new-test/
---
+++ AWS-Linux-Metrics-Task/final.sh
@@ -0,0 +1,4 @@
+PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/home/ubuntu/metrics
+export PATH
+/home/ubuntu/metrics/metricsnew.sh >> /home/ubuntu/metrics/metrics
+/home/ubuntu/metrics/awspush.sh
